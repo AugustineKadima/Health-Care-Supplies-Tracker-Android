25ad5c3255ee18edf6629757dfae957cd0e0d4b3
AugustineKadima <kadimasam@gmail.com>
2021-10-06 18:57:26 +0300

    Add donor signup

--- app/src/main/java/com/moringaschool/healthcaresuppliestracker/login_signup/DonorSignUp.java
+++ app/src/main/java/com/moringaschool/healthcaresuppliestracker/login_signup/DonorSignUp.java
@@ -2,15 +2,30 @@ package com.moringaschool.healthcaresuppliestracker.login_signup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.moringaschool.healthcaresuppliestracker.R;
 
 public class DonorSignUp extends AppCompatActivity {
 
+    TextView hospital_sign_up_link;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donor_sign_up);
+
+        hospital_sign_up_link = (TextView) findViewById(R.id.hospital_signup_link);
+        hospital_sign_up_link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DonorSignUp.this, HospitalSignUP.class));
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/moringaschool/healthcaresuppliestracker/login_signup/HospitalSignUP.java
+++ app/src/main/java/com/moringaschool/healthcaresuppliestracker/login_signup/HospitalSignUP.java
@@ -2,15 +2,28 @@ package com.moringaschool.healthcaresuppliestracker.login_signup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.moringaschool.healthcaresuppliestracker.R;
 
 public class HospitalSignUP extends AppCompatActivity {
 
+    TextView donor_sign_up_link;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hospital_sign_up);
+
+        donor_sign_up_link = (TextView) findViewById(R.id.donor_signup_link);
+        donor_sign_up_link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HospitalSignUP.this, DonorSignUp.class));
+            }
+        });
     }
 }
