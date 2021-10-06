f28079b7708ea6664a7283ade997e58a63f163f0
AugustineKadima <kadimasam@gmail.com>
2021-10-06 17:07:56 +0300

    Create splash screen

---
+++ app/src/main/java/com/moringaschool/healthcaresuppliestracker/login_signup/HospitalSignUP.java
@@ -0,0 +1,16 @@
+package com.moringaschool.healthcaresuppliestracker.login_signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.healthcaresuppliestracker.R;
+
+public class HospitalSignUP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospital_sign_up);
+    }
+}
