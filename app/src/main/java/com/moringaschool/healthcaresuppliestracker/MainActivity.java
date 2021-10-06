cd5b410d552b48d7d65003816ac8ab3c2fc435df
AugustineKadima <kadimasam@gmail.com>
2021-10-06 18:25:00 +0300

    Add hospital signup

--- app/src/main/java/com/moringaschool/healthcaresuppliestracker/MainActivity.java
+++ app/src/main/java/com/moringaschool/healthcaresuppliestracker/MainActivity.java
@@ -2,13 +2,28 @@ package com.moringaschool.healthcaresuppliestracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.moringaschool.healthcaresuppliestracker.login_signup.HospitalSignUP;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button get_started;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        get_started = (Button) findViewById(R.id.btn_get_started);
+
+        get_started.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, HospitalSignUP.class));
+            }
+        });
     }
 }
