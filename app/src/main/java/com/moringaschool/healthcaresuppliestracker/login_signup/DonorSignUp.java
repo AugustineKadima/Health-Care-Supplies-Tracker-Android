fb97bc4370b072feaf52151342edf361792a1e79
AugustineKadima <kadimasam@gmail.com>
2021-10-06 19:41:48 +0300

    Add intents to login activity

--- app/src/main/java/com/moringaschool/healthcaresuppliestracker/login_signup/DonorSignUp.java
+++ app/src/main/java/com/moringaschool/healthcaresuppliestracker/login_signup/DonorSignUp.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.moringaschool.healthcaresuppliestracker.R;
@@ -12,6 +13,7 @@ import com.moringaschool.healthcaresuppliestracker.R;
 public class DonorSignUp extends AppCompatActivity {
 
     TextView hospital_sign_up_link;
+    Button create_account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +28,12 @@ public class DonorSignUp extends AppCompatActivity {
             }
         });
 
-
+        create_account = (Button) findViewById(R.id.btn_donor_create_account);
+        create_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DonorSignUp.this, Login.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/moringaschool/healthcaresuppliestracker/login_signup/HospitalSignUP.java
+++ app/src/main/java/com/moringaschool/healthcaresuppliestracker/login_signup/HospitalSignUP.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.moringaschool.healthcaresuppliestracker.R;
@@ -12,6 +13,7 @@ import com.moringaschool.healthcaresuppliestracker.R;
 public class HospitalSignUP extends AppCompatActivity {
 
     TextView donor_sign_up_link;
+    Button create_account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,15 @@ public class HospitalSignUP extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(HospitalSignUP.this, DonorSignUp.class));
+            }
+        });
+
+        create_account = (Button) findViewById(R.id.btn_hospital_create_account);
+
+        create_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HospitalSignUP.this, Login.class));
             }
         });
     }
