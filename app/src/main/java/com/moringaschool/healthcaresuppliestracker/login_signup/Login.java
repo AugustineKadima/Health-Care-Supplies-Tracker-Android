61769b9b04c73dd082710043b5a2eed73111b5cf
AugustineKadima <kadimasam@gmail.com>
2021-10-06 19:31:59 +0300

    Add login Page

---
+++ app/src/main/java/com/moringaschool/healthcaresuppliestracker/login_signup/Login.java
@@ -0,0 +1,16 @@
+package com.moringaschool.healthcaresuppliestracker.login_signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.healthcaresuppliestracker.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
