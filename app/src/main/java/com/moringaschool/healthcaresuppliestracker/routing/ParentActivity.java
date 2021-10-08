3948aa204a88fe3bd927ba1eb207eaa811239593
AugustineKadima <kadimasam@gmail.com>
2021-10-08 23:42:02 +0300

    Add routing to navigate to the order fragment

--- app/src/main/java/com/moringaschool/healthcaresuppliestracker/routing/ParentActivity.java
+++ app/src/main/java/com/moringaschool/healthcaresuppliestracker/routing/ParentActivity.java
@@ -1,16 +1,71 @@
 package com.moringaschool.healthcaresuppliestracker.routing;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.moringaschool.healthcaresuppliestracker.R;
+import com.moringaschool.healthcaresuppliestracker.fragments.NewOrderFragment;
 
 public class ParentActivity extends AppCompatActivity {
+    TextView new_order;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        MenuItem item = menu.findItem(R.id.search_icon);
+        SearchView searchView = (SearchView) item.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        new_order =(TextView) findViewById(R.id.new_order_header);
+
+//      Navigate to the order fragment
+        new_order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newOrder();
+            }
+        });
+
+
+
     }
+
+    private void newOrder() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        NewOrderFragment newOrderFragment = new NewOrderFragment();
+        fragmentTransaction.add(R.id.fragment_container_pages, newOrderFragment);
+        fragmentTransaction.commit();
+    }
+
+
 }
