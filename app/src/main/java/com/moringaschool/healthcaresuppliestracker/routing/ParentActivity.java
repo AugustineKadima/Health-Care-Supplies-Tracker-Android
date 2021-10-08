cc45cbd3bae79623ee2c4b7395e0f8936aa2f29e
AugustineKadima <kadimasam@gmail.com>
2021-10-08 19:36:37 +0300

    Add parent activity

---
+++ app/src/main/java/com/moringaschool/healthcaresuppliestracker/routing/ParentActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.healthcaresuppliestracker.routing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.healthcaresuppliestracker.R;
+
+public class ParentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parent);
+    }
+}
