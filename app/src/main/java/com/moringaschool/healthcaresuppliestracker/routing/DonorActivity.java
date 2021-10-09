b2cfb7dcb1094e99a8ac0d31a8ef226d1d32d04d
AugustineKadima <kadimasam@gmail.com>
2021-10-10 01:52:15 +0300

    Add donor layout fragments and view model

--- app/src/main/java/com/moringaschool/healthcaresuppliestracker/routing/DonorActivity.java
+++ app/src/main/java/com/moringaschool/healthcaresuppliestracker/routing/DonorActivity.java
@@ -1,16 +1,60 @@
 package com.moringaschool.healthcaresuppliestracker.routing;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.moringaschool.healthcaresuppliestracker.R;
+import com.moringaschool.healthcaresuppliestracker.fragments.AllRequestsFragment;
+import com.moringaschool.healthcaresuppliestracker.fragments.DispatchesFragment;
+import com.moringaschool.healthcaresuppliestracker.fragments.InStockFragment;
 
 public class DonorActivity extends AppCompatActivity {
+    TextView _requests, _dispatches;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donor);
+
+        _requests = (TextView) findViewById(R.id.all_requests_header);
+        _dispatches = (TextView) findViewById(R.id.dispatches_header);
+
+
+        _requests.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                allRequests();
+            }
+        });
+
+        _dispatches.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatches();
+            }
+        });
+
+    }
+
+
+    private void allRequests() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        AllRequestsFragment allRequestsFragment = new AllRequestsFragment();
+        fragmentTransaction.add(R.id.fragment_container_donor_pages, allRequestsFragment);
+        fragmentTransaction.commit();
+    }
+
+    private void dispatches() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        DispatchesFragment dispatchesFragment = new DispatchesFragment();
+        fragmentTransaction.add(R.id.fragment_container_donor_pages, dispatchesFragment);
+        fragmentTransaction.commit();
     }
 }
