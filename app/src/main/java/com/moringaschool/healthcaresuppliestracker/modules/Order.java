430d71b055d7d81f4d1faf4bf298f7f6ce734960
AugustineKadima <kadimasam@gmail.com>
2021-10-10 13:49:38 +0300

    Create oder model and adapter

---
+++ app/src/main/java/com/moringaschool/healthcaresuppliestracker/modules/Order.java
@@ -0,0 +1,32 @@
+package com.moringaschool.healthcaresuppliestracker.modules;
+
+public class Order {
+
+    private String itemName;
+    private String itemQuantity;
+    private String itemDescription;
+    private String donorEmail;
+
+    public Order(String itemName, String itemQuantity, String itemDescription, String donorEmail) {
+        this.itemName = itemName;
+        this.itemQuantity = itemQuantity;
+        this.itemDescription = itemDescription;
+        this.donorEmail = donorEmail;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getItemQuantity() {
+        return itemQuantity;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public String getDonorEmail() {
+        return donorEmail;
+    }
+}
